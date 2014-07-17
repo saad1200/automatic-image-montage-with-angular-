ee477df78b177b457a8d79a08d6f8d08667b2914
Saad Sawari <saad1200@gmail.com>
2014-07-17 02:16:46 +0100

    added bootstrap modal

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@
     function dashboardController() {
         var vm = this;
         vm.posts = [];
+        vm.selectedPost = {};
         activate();
 
         function activate() {
             
             for(var i = 1; i<73;i++){
-                vm.posts.push({imageUrl: 'public/images/'+ i +'.jpg'});
+                vm.posts.push({ header: 'Image header', body: 'Image body', imageUrl: 'public/images/'+ i +'.jpg'});
             }
         };
     
+        vm.show = function(index){
+            vm.selectedPost = vm.posts[index];
+            
+            $('#myModal').modal({
+              keyboard: false
+            })
+        };
     }
 
 })();
